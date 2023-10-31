19088106e403382babd6a362675e9cb9b6973d10
masud1989 <mranasrj@gmail.com>
2023-10-31 17:32:23 +0600

    api-call

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,32 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  
+  const [data, setData] = useState([])
+
+  useEffect( ()=> {
+    (async () => {
+      let  res = await fetch('https://dummyjson.com/products')
+      let jsonData = await res.json()
+      setData(jsonData['products'])
+    })()
+  }, [])
+  
+
   return (
     <div>
       <h1>This is Home Page</h1>
+      {
+        data.map( (item, index) =>{
+          return <div key={index}>
+            <h1>{item.title}</h1>
+            <h1>{item.price}</h1>
+            <p>{item.description}</p>
+          </div>
+        })
+      }
     </div>
   )
 }
